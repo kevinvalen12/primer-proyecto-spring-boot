cf4d9c71c5bd351591648eab2aa0b93b2431c0f8
kevinvalen12 <valenciakevin894@gmail.com>
2026-07-09 21:28:47 -0500

    first commit

---
+++ src/main/java/prueba/Primer_String_Boot/PrimerStringBootApplication.java
@@ -0,0 +1,13 @@
+package prueba.Primer_String_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerStringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerStringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba/Primer_String_Boot/PrimerStringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.Primer_String_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerStringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
